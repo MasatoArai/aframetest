cd89e73e9ce0d9aecb81d66131e547fc3c695aca
MASATO ARAI <ponponsippo@gmail.com>
2017-01-27 17:54:36 +0900

    syuusei1

--- script/main.js
+++ script/main.js
@@ -1,1 +1,18 @@
 var app;
+var mylatlon={lat:0,lon:0,heading:0};
+var tokyotowerLatLon = {lat:35.6585618,lon:139.7453056};
+if( navigator.geolocation )
+{
+	// 現在位置を取得できる場合の処理
+    navigator.geolocation.getCurrentPosition( function(e){
+        if(!e)return;
+        mylatlon.lat = e.coords.latitude;
+        mylatlon.lon = e.coords.longitude;
+    } , function(e){
+        console.error("取得失敗");
+    } , {
+        enableHighAccuracy:true,
+        timeout:10000,
+        maximumAge:50000
+    } ) ;
+}
