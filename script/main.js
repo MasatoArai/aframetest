12b82b08610b41d9c4696302ad72058225530f38
MASATO ARAI <ponponsippo@gmail.com>
2017-01-30 11:50:12 +0900

    notr

--- script/main.js
+++ script/main.js
@@ -6,6 +6,21 @@ var mainCam={};
 
 
 document.addEventListener('DOMContentLoaded',function(event){
+    
+    var consoleDiv = document.querySelector('#console');
+      window.addEventListener('deviceorientation', function(event) {
+        var compassdir;
+        if(event.webkitCompassHeading) {
+          // Apple works only with this, alpha doesn't work
+          compassdir = event.webkitCompassHeading;  
+        }
+            //var north = compassHeading(event.webkitCompassHeading,event.beta,event.gamma);
+            //consoleDiv.innerHTML = north;
+            consoleDiv.innerHTML = compassdir;
+      });
+    
+    
+    
     mainCam = document.querySelector('#basecam');
     mainCam.addEventListener('loaded',function(ev){
         window.ondeviceorientation = function(event) {
@@ -14,6 +29,9 @@ document.addEventListener('DOMContentLoaded',function(event){
             console.log(w);
             consoleDiv.innerHTML = w;
         }
+        
+        
+        
         if( navigator.geolocation ){
             // 現在位置を取得できる場合の処理
             navigator.geolocation.getCurrentPosition( function(e){
@@ -48,7 +66,6 @@ document.addEventListener('DOMContentLoaded',function(event){
                 var tokyotower = document.querySelector('#tokyotowerModel');
                 var base = document.querySelector('#tokyotowerBase');
                 var arrowfoot = document.querySelector('#arrowfoot');
-                var consoleDiv = document.querySelector('#console');
                 //tokyotower.setAttribute('position',{x:x,y:0,z:y});
                 tokyotower.setAttribute('position',{x:0,y:0,z:-distance});
                 base.setAttribute('rotation',{x:0,y:-direct,z:0});
