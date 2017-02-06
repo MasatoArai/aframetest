aeb19d5ee5d64ca2367942248f864d23e697add0
neko nekon <ponponsippo@gmail.com>
2017-02-06 14:52:06 +0900

    test

--- script/main.js
+++ script/main.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded',function(event){
 
     var distance = google.maps.geometry.spherical.computeDistanceBetween(NTT, TOKYOTOWER);
     var direct =  google.maps.geometry.spherical.computeHeading(NTT, TOKYOTOWER);
-
+    consoleDiv.innerHTML = "test";
     $(window).on('deviceorientation',function(ev){
         var compassdir=ev.originalEvent.webkitCompassHeading||ev.originalEvent.alpha;
         compassdir=(360+(compassdir+orientation))%360;
